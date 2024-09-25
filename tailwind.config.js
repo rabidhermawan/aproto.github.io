d9dfd49656e737006f4408380e834e42b205dd15
rabidhermawan <97014247+rabidhermawan@users.noreply.github.com>
2024-09-25 23:07:57 +0700

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./quiz1.html", 
+    "./pages/**/*.html",
+    "./quiz1/**/*.html",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        rocksalt: ["Rock Salt"],
+        plexmono: ["Courier Prime"],
+        playwrigtcl: ["Playwrite CL"],
+        vibur: ["Vibur"],
+      }
+    },
+  },
+  plugins: [],
+}
+
