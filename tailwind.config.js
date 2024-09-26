4d0f984c72bafa4ef40aefd475749792a944bace
rabidhermawan <97014247+rabidhermawan@users.noreply.github.com>
2024-09-26 09:34:36 +0700

    Hotfix for the homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    ".index.html",
-    ".quiz1.html", 
+    "..index.html",
+    "./quiz1.html", 
     "./pages/**/*.html",
     "./quiz1/**/*.html",
   ],
